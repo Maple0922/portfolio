ae4a5b3a69800e5e7e535d6f43f0d1fe2ecfdd58
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2021-01-18 05:33:51 +0900

    Update: function to calc age

--- src/components/Main/Profile.vue
+++ src/components/Main/Profile.vue
@@ -40,22 +40,22 @@ export default {
         {
           name: "user",
           icon: "UserSvg",
-          text: "中島楓人(なかじまふうと)",
+          text: "中島楓人 (なかじまふうと)",
         },
         {
           name: "calendar",
           icon: "CalendarSvg",
-          text: "1999年9月22日生まれ(21歳)",
+          text: `1999年9月22日生まれ (${this.calcAge()}歳)`,
         },
         {
           name: "pin",
           icon: "PinSvg",
-          text: "東京都杉並区在住",
+          text: "東京都杉並区 在住",
         },
         {
           name: "univ",
           icon: "UnivSvg",
-          text: "明治大学総合数理学部在学中(24卒)",
+          text: "明治大学 総合数理学部 在学中(24卒)",
         },
         {
           name: "comment",
@@ -70,6 +70,35 @@ export default {
   methods: {
     makeSvgUrl(name) {
       return "../../assets/images/" + name + ".svg";
+    },
+
+    calcAge() {
+      const myBirthDay = {
+        year: 1999,
+        month: 9,
+        date: 22,
+      };
+
+      const birthDate = new Date(
+        myBirthDay.year,
+        myBirthDay.month - 1,
+        myBirthDay.date
+      );
+
+      const y2 = birthDate.getFullYear().toString().padStart(4, "0");
+      const m2 = (birthDate.getMonth() + 1).toString().padStart(2, "0");
+      const d2 = birthDate.getDate().toString().padStart(2, "0");
+
+      const today = new Date();
+      const y1 = today.getFullYear().toString().padStart(4, "0");
+      const m1 = (today.getMonth() + 1).toString().padStart(2, "0");
+      const d1 = today.getDate().toString().padStart(2, "0");
+
+      const age = Math.floor(
+        (Number(y1 + m1 + d1) - Number(y2 + m2 + d2)) / 10000
+      );
+
+      return age;
     },
   },
 
