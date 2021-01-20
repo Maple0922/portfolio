db08c970411d04b04a2523ded8b4de775d8fbf5c
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2021-01-20 18:04:21 +0900

    Update: skills page

--- src/components/Main/Skills.vue
+++ src/components/Main/Skills.vue
@@ -1,25 +1,51 @@
 <template>
-  <section class="main__container skills" id="skills">
-    <h2 class="main__container__title" :class="{ show: show }">
+  <section class="main__container skills" id="skills" :class="{ show: show }">
+    <h2 class="main__container__title">
       <p class="main__container__title--en">Skills</p>
       <p class="main__container__title--ja">スキル</p>
     </h2>
-    <div class="skills__container" v-for="(skill, key) in skills" :key="key">
-      <h3 class="skills__category">{{ skill.category }}</h3>
-      <ul class="skills__list">
-        <li
-          class="skills__list__item"
-          v-for="(item, key) in skill.items"
-          :key="key"
-        >
-          {{ item.name }}
-        </li>
-      </ul>
+    <span class="caption">※太字下線は実務経験あり</span>
+    <div class="skills__wrapper">
+      <div
+        class="skills__container"
+        v-for="(skill, firstKey) in skills"
+        :key="firstKey"
+      >
+        <h3 class="skills__category">{{ skill.category }}</h3>
+        <ul class="skills__list">
+          <li
+            class="skills__list__item"
+            v-for="(item, secondKey) in skill.items"
+            :key="secondKey"
+          >
+            <p class="skills__list__item__name" :class="{ bold: item.ex }">
+              {{ item.name }}
+            </p>
+            <p class="skills__list__item__stars">
+              <component
+                class="skills__list__item__star"
+                v-for="n of item.star"
+                :key="`n-index-${n}`"
+                :is="'StarFillSvg'"
+              />
+              <component
+                class="skills__list__item__star"
+                v-for="m of 5 - item.star"
+                :key="`m-index-${m}`"
+                :is="'StarEmptySvg'"
+              />
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import StarFillSvg from "@/assets/images/svg/star_fill.svg";
+import StarEmptySvg from "@/assets/images/svg/star_empty.svg";
+
 export default {
   name: "Skills",
   data() {
@@ -36,17 +62,17 @@ export default {
             },
             {
               name: "CSS3",
-              star: 4,
+              star: 3,
               ex: true,
             },
             {
               name: "JavaScript",
-              star: 4,
+              star: 3,
               ex: true,
             },
             {
               name: "PHP",
-              star: 4,
+              star: 2,
               ex: true,
             },
           ],
@@ -57,61 +83,76 @@ export default {
             {
               name: "Sass(SCSS)",
               star: 3,
-              ex: false,
-            },
-            {
-              name: "Vue.js",
-              star: 3,
-              ex: false,
+              ex: true,
             },
             {
               name: "jQuery",
               star: 3,
-              ex: false,
+              ex: true,
             },
             {
-              name: "Angular",
-              star: 3,
+              name: "Vue.js",
+              star: 2,
               ex: false,
             },
             {
               name: "Laravel",
-              star: 3,
+              star: 2,
+              ex: true,
+            },
+            {
+              name: "React",
+              star: 1,
               ex: false,
+            },
+            {
+              name: "Angular",
+              star: 1,
+              ex: true,
             },
           ],
         },
         {
-          category: "ツール",
+          category: "開発ツール等",
           items: [
             {
               name: "Git(Github)",
               star: 3,
-              ex: false,
+              ex: true,
             },
             {
-              name: "Gulp",
-              star: 3,
+              name: "node.js",
+              star: 2,
               ex: false,
             },
             {
               name: "Webpack",
-              star: 3,
+              star: 2,
+              ex: true,
+            },
+            {
+              name: "Gulp",
+              star: 2,
               ex: false,
             },
             {
               name: "Wordpress",
-              star: 3,
+              star: 2,
               ex: false,
             },
             {
               name: "MySQL",
-              star: 3,
+              star: 2,
               ex: false,
             },
             {
               name: "SQLite",
-              star: 3,
+              star: 2,
+              ex: true,
+            },
+            {
+              name: "GraphQL",
+              star: 1,
               ex: false,
             },
           ],
@@ -120,44 +161,38 @@ export default {
           category: "その他",
           items: [
             {
-              name: "Atom",
-              star: 3,
-              ex: false,
-            },
-            {
               name: "Figma",
               star: 3,
               ex: false,
             },
             {
               name: "Slack",
-              star: 3,
-              ex: false,
+              star: 4,
+              ex: true,
             },
             {
               name: "Chatwork",
               star: 3,
-              ex: false,
+              ex: true,
             },
             {
               name: "Asana",
               star: 3,
-              ex: false,
+              ex: true,
             },
             {
               name: "Trello",
               star: 3,
-              ex: false,
-            },
-            {
-              name: "XSERVER",
-              star: 3,
-              ex: false,
+              ex: true,
             },
           ],
         },
       ],
     };
+  },
+  components: {
+    StarFillSvg,
+    StarEmptySvg,
   },
   created() {
     window.addEventListener("scroll", this.handleScroll);
@@ -169,7 +204,7 @@ export default {
     handleScroll() {
       if (!this.show) {
         var top = this.$el.getBoundingClientRect().top;
-        this.show = top < window.innerHeight - 100;
+        this.show = top < window.innerHeight - 300;
       }
     },
   },
@@ -179,23 +214,91 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/scss/import";
 .skills {
-  &__container {
+  .caption {
+    font-size: 13px;
+    margin-bottom: 16px;
     display: block;
+    width: 100%;
+    padding-right: 32px;
+    text-align: right;
+  }
+  &__wrapper {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  &__container {
+    background: transparentize($white, 0.4);
+    border-radius: 4px;
+    box-shadow: 0 1px 3px 0px $light-gray;
+    padding: 20px 24px 24px;
+
+    @include mq("pc") {
+      width: calc(50% - 30px);
+      margin: 0 15px;
+      &:nth-child(-n + 2) {
+        margin-bottom: 30px;
+      }
+    }
+    @include mq("sp") {
+      width: 100%;
+      margin-bottom: 30px;
+    }
   }
 
   &__category {
     display: block;
+    font-size: 18px;
+    margin-bottom: 16px;
+    margin-left: 8px;
   }
 
   &__list {
+    border-right: 1px solid $main-background;
+    border-left: 1px solid $main-background;
     &__item {
-      display: inline-block;
-      padding: 2px 8px;
-      margin: 2px;
-      font-size: 12px;
-      color: $white;
-      background: blue;
-      border-radius: 8px;
+      padding: 10px 0;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      border-top: 1px solid $super-light-gray;
+
+      @include mq("pc") {
+        font-size: 15px;
+      }
+
+      @include mq("sp") {
+        font-size: 13px;
+      }
+      &:nth-child(2n-1) {
+        // background: $light-gray;
+        background: $main-background;
+      }
+      &:last-child {
+        border-bottom: 1px solid $super-light-gray;
+      }
+      &__name {
+        width: 50%;
+        padding-left: 10px;
+
+        &.bold {
+          font-weight: bolder;
+          text-decoration: underline;
+        }
+      }
+      &__stars {
+        display: flex;
+        width: 50%;
+        justify-content: flex-end;
+        padding-right: 10px;
+      }
+      &__star {
+        display: inline-block;
+        margin-right: 3px;
+        width: 18px;
+        height: 18px;
+      }
     }
   }
 }
